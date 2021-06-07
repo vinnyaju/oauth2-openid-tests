f96ed35e2cfcb5a2c6f293edce048d3510352e49
Marcus Vinícius Rodrigues Andrade <vinnyaju@gmail.com>
2021-06-07 08:13:33 -0300

    Iniciando a implementação do client

--- src/client/main.go
+++ src/client/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var oauth = struct {
+	authURL string
+}{
+	authURL: "http://192.168.100.101:8080/auth/realms/learningApp/protocol/openid-connect/auth",
+}
 
 func main() {
 	fmt.Println("OK")
+	http.HandleFunc("/", home)
+	http.HandleFunc("/login", login)
+	http.ListenAndServe(":3000", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("template/index.html"))
+	t.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	req, err := http.NewRequest("GET", oauth.authURL, nil)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	//req.URL.RawQuery = "state=123abc&client_id=billingApp&response_type=code"
+	qs := url.Values{}
+	qs.Add("state", "123abc")
+	qs.Add("client_id", "billingApp")
+	qs.Add("response_type", "code")
+
+	req.URL.RawQuery = qs.Encode()
+	http.Redirect(w, r, req.URL.String(), http.StatusFound)
 }
